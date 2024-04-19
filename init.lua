650538d07a04bd95ef21d35aaba427bf83e23ed1
ablaspinto <ablaspinto@gmail.com>
2024-04-19 09:10:18 -0400

    refactored file structure

--- init.lua
+++ init.lua
@@ -1,10 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
--- or                              , branch = '0.1.x',
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.cmd("set termguicolors")
-vim.g.mapleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -19,35 +12,5 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-  {
-    'whatyouhide/vim-gotham',
-    dependencies = {"rktjmp/lush.nvim"},
-  },
-  {
-    "ThePrimeagen/harpoon",
-    branch = "harpoon2",
-    dependencies = { "nvim-lua/plenary.nvim" }
-  },
-   {
-    'nvim-telescope/telescope.nvim', tag = '0.1.6',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-    {
-      {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
-    }
-}
-
-
-local opts = {}
-require("lazy").setup(plugins, opts)
-local builtin = require("telescope.builtin")
-vim.keymap.set('n','<leader>fg',builtin.live_grep,{})
-vim.keymap.set('n','<C-p>',builtin.find_files,{})
-local config = require("nvim-treesitter.configs")
-config.setup({
-  ensure_installed = {"lua","javascript"},
-  highlight = {enable = true},
-  indent = {enable =true },
-})
-vim.cmd.colorscheme "gotham"
+require("vim-options")
+require("lazy").setup("plugins")
---
+++ lua/plugins/gotham.lua
@@ -0,0 +1,8 @@
+return  {
+    'whatyouhide/vim-gotham',
+    dependencies = {"rktjmp/lush.nvim"},
+    config = function()
+    vim.cmd.colorscheme "gotham"
+  end
+}
+
